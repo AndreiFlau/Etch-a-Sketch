bec465a8fbb13d9c0749c54c8b810bc953ddef31
Andrei <andreiflausino@live.com>
2024-04-15 19:15:00 -0300

    Finished Project

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let container = document.querySelector(".container");
 
 function createDivs(number) {
+  // container.style.width = `${number * 30}px`;
+  const pixelSize = 800 / number;
   for (let i = 0; i < number * number; i++) {
     let div = document.createElement("div");
+    div.style.height = `${pixelSize}px`;
+    div.style.width = `${pixelSize}px`;
     container.appendChild(div);
   }
 }
@@ -16,13 +20,14 @@ function hoverEffect() {
       const randomB = Math.floor(Math.random() * 255);
       const randomC = Math.floor(Math.random() * 255);
 
-
-      if (div.style.backgroundColor !== "" && parseFloat(div.style.opacity) < 1) {
-        div.style.opacity = parseFloat(div.style.opacity) + 0.1;
+      if (div.style.backgroundColor !== "" && parseFloat(div.style.opacity) <
+        1) {
+        div.style.opacity = `${parseFloat(div.style.opacity) + 0.1}`;
       }
       else if (div.style.backgroundColor === "") {
-        div.style.opacity = 0.1;
-        div.style.backgroundColor = 'rgb(' + randomA + ',' + randomB + ',' + randomC + ')';
+        div.style.opacity = `0.1`;
+        div.style.backgroundColor = "rgb(" + randomA + "," + randomB + "," +
+          randomC + ")";
       }
 
       // setTimeout(() =>{
@@ -39,11 +44,10 @@ function hoverEffect() {
 createDivs(16);
 hoverEffect();
 
-
 const changeGrid = document.querySelector("#gridBtn");
 const modal = document.querySelector(".gridDialog");
 const confirmBtn = document.querySelector("#confirmBtn");
-const cancelBtn = document.querySelector("#cancelBtn");
+const resetBtn = document.querySelector("#resetBtn");
 const modalForm = document.querySelector("#squareNums");
 
 let numOfSquares = 0;
@@ -51,13 +55,36 @@ changeGrid.addEventListener("click", () => {
   modal.showModal();
 });
 
-confirmBtn.addEventListener("click", () => {
-  if (modalForm.value > 0 && modalForm.value < 100) {
+confirmBtn.addEventListener("click", (e) => {
+  if (modalForm.value <= 0) {
+    alert("The value must be above 0!!!");
+    e.preventDefault();
+    return false;
+  }
+  else if (modalForm.value > 100) {
+    alert("Value can't be bigger than 100!!!");
+    e.preventDefault();
+    return false;
+  }
+  else {
     numOfSquares = modalForm.value;
     while (container.firstChild) {
       container.removeChild(container.firstChild);
     }
     createDivs(numOfSquares);
+  }
+  hoverEffect();
+});
+
+resetBtn.addEventListener("click", () => {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  if (numOfSquares !== 0) {
+    createDivs(numOfSquares);
+  }
+  else {
+    createDivs(16);
   }
   hoverEffect();
 });
